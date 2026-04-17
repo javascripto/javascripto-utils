5c8a417b13995d196dc2036ccc3ddae802e557f1
Yuri Alves <yurialves2@gmail.com>
2026-04-16 22:01:02 -0300

    adiciona verificação para garantir que min seja menor ou igual a max na função randomInt

--- src/utils/random-int.ts
+++ src/utils/random-int.ts
@@ -6,5 +6,6 @@ export function randomInt({
   min = defaultArgs.min,
   max = defaultArgs.max,
 }: RandomIntArgs = defaultArgs): number {
+  if (min > max) throw new Error('min should be less than or equal to max');
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
