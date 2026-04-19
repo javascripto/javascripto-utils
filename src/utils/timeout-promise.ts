1259dc0f9adfe871c11f57b15e05d787642f5ec1
Yuri Alves <yurialves2@gmail.com>
2026-04-19 06:43:33 -0300

    adiciona função timeoutPromise e classe PromiseTimeoutError para gerenciamento de tempo de execução de promessas

---
+++ src/utils/timeout-promise.ts
@@ -0,0 +1,22 @@
+
+export function timeoutPromise<T = unknown>(
+  promise: Promise<T>,
+  milliseconds: number,
+): Promise<T> {
+  return Promise.race([
+    promise,
+    new Promise<T>((_, reject) => {
+      setTimeout(
+        () => reject(new PromiseTimeoutError(milliseconds)),
+        milliseconds,
+      );
+    }),
+  ]);
+}
+
+export class PromiseTimeoutError extends Error {
+  constructor(milliseconds: number) {
+    super(`Promise timed out after ${milliseconds} milliseconds`);
+    this.name = 'PromiseTimeoutError';
+  }
+}
